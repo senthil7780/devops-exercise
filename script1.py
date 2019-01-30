111a053449833fb1d6689bf356ff3670cd7ba4e4
Senthil K <senthilkumar.kannappan@gmail.com>
2019-01-30 15:54:40 +0800

    added username & date

--- script1.py
+++ script1.py
@@ -1,9 +1,11 @@
+from __future__ import print_function
 import requests
 import json
 import sys
 import urllib
 #import json
 from pprint import pprint
+
 
 ####
 # inputs
@@ -31,13 +33,26 @@ gh_session.auth = (username, token)
 for item in data:
    item = item.split("/")
    orgname = item[0]
-   reponame = item[1]
-   urltest = 'https://api.github.com/orgs/{}/repos'
+   reponame = item[1].strip("\n")
+   repos_url = 'https://api.github.com/orgs/{a}/repos'
+   commits_url = 'https://api.github.com/repos/{a}/{b}'+'/commits'
+   commit = 'commits'
    #url = 'https://api.github.com/orgs/'+ orgname + '/repos'
-   print urltest.format(orgname)
-   repos = json.loads(gh_session.get(urltest.format(orgname)).text)
+   print(repos_url.format(a=orgname))
+   print(commits_url.format(a=orgname, b=reponame,))
+   repos = json.loads(gh_session.get(repos_url.format(a=orgname)).text)
    #pprint(repos)
 
    for repo in repos:
-      #print repo
-      print("Name of the repo: %s Clone URL: %s " % (repo['name'], repo['clone_url']))
+      repo_name = repo['name']
+      clone_url = repo['clone_url']
+      print(repo_name, clone_url)
+      commits = json.loads(gh_session.get(commits_url.format(a=orgname, b=reponame)).text)
+      #for commit in commits:
+      print(commits[0]["commit"]["committer"]["name"])
+      print(commits[0]["commit"]["committer"]["date"])
+      #print(author_name)
+         #print repo
+         #print("Date of the latest commit : %s , Name of the latest Author : %s" % (data[commit[0]['date']],data[commit[0]['author']]))
+         #pprint(commit[0].['author'].['name'])
+         #print("Name of the repo: %s, Clone URL: %s " % repo_name, clone_url)
