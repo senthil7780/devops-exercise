b42db036a3d94215610e99ea4a90b1973d34bfc0
Senthil K <senthilkumar.kannappan@gmail.com>
2019-01-30 18:37:24 +0800

    csv file write completed

--- access_github_details.py
+++ access_github_details.py
@@ -1,58 +1,58 @@
-from __future__ import print_function
 import requests
 import json
 import sys
 import urllib
-#import json
+import csv
 from pprint import pprint
-
-
-####
-# inputs
-####
+   
+data = []
+github_dic ={}
 
 username=raw_input("Please enter your github id : ")
 
 # from https://github.com/user/settings/tokens
 token=raw_input("Please enter your OAuth Token : ")
-#'c056b0a0677e0869d03d449040dc5167ac156898'
-
 
 print("please enter the repos in $orgname/$reponame format")
-data = []
 for line in sys.stdin:
     data.append(line)
-
-#repos_url = 'https://api.github.com/user/repos'
 
 # create a re-usable session object with the user creds in-built
 gh_session = requests.Session()
 gh_session.auth = (username, token)
 
-# get the list of repos belonging to me
+# get the list of repos for the given org & get the commit history to find the Latest Author & date
 for item in data:
    item = item.split("/")
    orgname = item[0]
    reponame = item[1].strip("\n")
+# Build the Github API URLs
    repos_url = 'https://api.github.com/orgs/{a}/repos'
    commits_url = 'https://api.github.com/repos/{a}/{b}'+'/commits'
-   commit = 'commits'
-   #url = 'https://api.github.com/orgs/'+ orgname + '/repos'
-   print(repos_url.format(a=orgname))
-   print(commits_url.format(a=orgname, b=reponame,))
+# Acces the apis
    repos = json.loads(gh_session.get(repos_url.format(a=orgname)).text)
-   #pprint(repos)
-
+   i=0
    for repo in repos:
       repo_name = repo['name']
       clone_url = repo['clone_url']
-      print(repo_name, clone_url)
       commits = json.loads(gh_session.get(commits_url.format(a=orgname, b=reponame)).text)
-      #for commit in commits:
-      print(commits[0]["commit"]["committer"]["name"])
-      print(commits[0]["commit"]["committer"]["date"])
-      #print(author_name)
-         #print repo
-         #print("Date of the latest commit : %s , Name of the latest Author : %s" % (data[commit[0]['date']],data[commit[0]['author']]))
-         #pprint(commit[0].['author'].['name'])
-         #print("Name of the repo: %s, Clone URL: %s " % repo_name, clone_url)
+      author_name = commits[0]["commit"]["committer"]["name"]
+      latest_commit_date = commits[0]["commit"]["committer"]["date"]
+      print("Repo Name : {0}, Clone URL : {1}, Author Name : {2}, Commit Date : {3} ".format(repo_name, clone_url, author_name, latest_commit_date))
+      values = [repo_name, clone_url, author_name, latest_commit_date]
+      github_dic[i]=values
+      i=i+1
+
+   with open("file.csv","a") as csv_file:
+      csv_writer = csv.writer(csv_file)
+      for i in github_dic :
+         list = github_dic[i]
+         print list
+         csv_writer.writerow(list)
+      
+print(github_dic)
+csv_file.close()
+
+
+
+#writeFile.close()
